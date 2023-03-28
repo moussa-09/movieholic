441e9b260bc611f20dded639b0e23aca72b0b285
p40327sm <steven.moussa@student.manchester.ac.uk>
2023-03-28 22:06:57 +0100

    deployment

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 
 if(ENV === 'production'){
     app.use(express.static(path.join(__dirname, '../build')));
-    app.use((req, res) => {
+    app.get((req, res) => {
         res.sendFile(path.join(__dirname, '../build/index.html'));
     })
 }
