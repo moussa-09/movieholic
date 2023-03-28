42db21dbfe8378dbe75b19497879c0670d5e6b1f
p40327sm <steven.moussa@student.manchester.ac.uk>
2023-03-28 20:30:49 +0100

    deployment

--- src/setUpProxy.js
+++ src/setUpProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: `http://localhost:${process.env.port || 5000}`,
+      target: `https://movieholic.onrender.com/${process.env.port || 5000}`,
       changeOrigin: true,
     })
   );
